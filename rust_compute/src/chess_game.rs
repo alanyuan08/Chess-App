e50e854660530e94cccbbe20c02d7c71fa869b9a
alanyuan08 <alan0408yuan@gmail.com>
2026-06-27 15:29:44 -0400

    clamp

--- rust_compute/src/chess_game.rs
+++ rust_compute/src/chess_game.rs
@@ -17,7 +17,7 @@ pub const MAX_DEPTH: i32 = 20;
 // termination signal to the other threads.
 
 // The other threads the single every 2048 executions
-pub const DEPTH_SEARCH_LIMIT: u64 = 25;
+pub const DEPTH_SEARCH_LIMIT: u64 = 15;
 
 pub const INFINITY: i32 = 32000;
 pub const MATE_VALUE: i32 = 30000;
