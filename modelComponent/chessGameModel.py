e1dc0c913a75bc87262a4d0ca1f9c4f80caed553
alanyuan08 <alan0408yuan@gmail.com>
2026-06-29 12:57:05 -0400

    UCI communications

--- modelComponent/chessGameModel.py
+++ modelComponent/chessGameModel.py
@@ -80,5 +80,4 @@ class ChessGameModel():
         
         # Rust Compute Next Move
         uci_move = self.game_engine.compute_next_move(self.returnChessMoves())
-        print(uci_move)
         return self.chessBoard.uci_to_move_command(uci_move)
