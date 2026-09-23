43c3fc913c85b89924c9665e531a244d3ff01375
alanyuan08 <alan0408yuan@gmail.com>
2026-09-23 15:17:31 -0700

    test

--- rust_compute/src/chess_game.rs
+++ rust_compute/src/chess_game.rs
@@ -17,14 +17,14 @@ use crate::nnue_network::*;
 use crate::search_command::*;
 use crate::move_command::*;
 
-pub const PV_DEPTH: i32 = 15;
-pub const MAX_DEPTH: i32 = 20;
+pub const PV_DEPTH: i32 = 18;
+pub const MAX_DEPTH: i32 = 22;
 
 // When a thread finishes, if it exceeds the time, it will send the 
 // termination signal to the other threads.
 
 // The other threads the single every 2048 executions
-pub const SEARCH_TIME_LIMIT: u64 = 25;
+pub const SEARCH_TIME_LIMIT: u64 = 15;
 
 pub const INFINITY: i32 = 32000;
 pub const MATE_VALUE: i32 = 30000;
@@ -37,7 +37,7 @@ pub const MATE_THRESHOLD: i32 = MATE_VALUE - (MAX_DEPTH * 2);
 pub const NUM_THREADS: usize = 8;
 
 // Condon-Thompson Bucket Transposition Table
-pub const CACHE_SIZE: usize = 64;
+pub const CACHE_SIZE: usize = 16;
 
 // Model path
 pub const MODEL_PATH: &str = "nnue-training/nnue_weights.bin";
